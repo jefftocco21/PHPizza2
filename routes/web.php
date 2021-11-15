ddac6c257483d74fd576a5d51213c53d83840a32
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-11-15 15:02:18 -0500

    Using Cashing for Expensive Operations

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,14 @@ Route::get('/', function () {
 
 Route::get('posts/{post}', function($slug) { //wildcard is put in brackets, matched and passed to function
 
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if(! file_exists($path)){
+    if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
      return redirect('/'); //go to homepage if file does not exist
     }
 
-    $post = file_get_contents($path);
+    //var_dump('file_get_contents'); //would dump the string when the string is no longer stored in cash
+    $post = cache()->remember("posts.{$slug}", 5, function() use ($path){ //remember stores key in cache, interval in seconds, then pass function
+        return $post = file_get_contents($path);
+    });
 
-    return view('post', [
-        'post' => $post
-    ]);
+    return view('post', ['post' => $post]);
 })->where('post', '[A-z_\-]+'); //404 would be thrown if URI does not match regular expression
