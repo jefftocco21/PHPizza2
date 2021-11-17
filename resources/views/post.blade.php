e6874f4a1b44a4f05135aaea7b1283f51c3119b8
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-11-17 13:13:32 -0500

    A Few Tweaks and Consideration

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,12 @@
-@extends('layout')
+<x-layout>
 
-@section('content')
-<article>
-        <h1>{!!$post->title!!} </h1>
+    <article>
+        <h1>{{$post->title}} </h1>
 
         <div>
             {!! $post->body !!}
         </div>
     </article>
     <a href="/">Go Back</a>
-@endsection
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,8 @@ Route::get('/', function () {
 //wildcard is put in brackets, matched and passed to function
 Route::get('posts/{post}', function($slug) { 
     //Find a post by its slug and pass it to a view called "post"
-    $post = Post::find($slug);
-    ddd($post);
 
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::findorFail($slug)
     ]);
 }); //->where('post', '[A-z_\-]+'); //404 would be thrown if URI does not match regular expression
