41f332ee2bb892d4d86a1269b0ae12ead7f5c2eb
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-11-23 15:53:38 -0500

    Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'body' => '
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim accumsan orci at mattis. Aliquam eget lectus vitae nisl varius auctor eu tincidunt ex. Nam luctus nisl interdum turpis blandit, finibus convallis eros rutrum. Nunc tempus posuere dui ac dictum. Ut sollicitudin mi tortor, euismod tristique diam iaculis et. Nam molestie id enim in tincidunt. Phasellus nisl quam, placerat sit amet velit ac, suscipit blandit tellus. Integer consectetur sollicitudin massa. Phasellus hendrerit mattis velit ut sodales. Quisque porta lacinia orci vitae commodo. Praesent eget tempus mauris, vel varius ante. Nunc nulla lacus, tempus vitae tempor eget, commodo in tortor.</p>'
+        ]);
+        
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-second-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'body' => '
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim accumsan orci at mattis. Aliquam eget lectus vitae nisl varius auctor eu tincidunt ex. Nam luctus nisl interdum turpis blandit, finibus convallis eros rutrum. Nunc tempus posuere dui ac dictum. Ut sollicitudin mi tortor, euismod tristique diam iaculis et. Nam molestie id enim in tincidunt. Phasellus nisl quam, placerat sit amet velit ac, suscipit blandit tellus. Integer consectetur sollicitudin massa. Phasellus hendrerit mattis velit ut sodales. Quisque porta lacinia orci vitae commodo. Praesent eget tempus mauris, vel varius ante. Nunc nulla lacus, tempus vitae tempor eget, commodo in tortor.</p>'
+        ]);
     }
 }
