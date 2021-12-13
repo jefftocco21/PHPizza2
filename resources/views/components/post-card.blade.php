82e8df5eefe13fdbe41d39b6adaace94d235ef09
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-12-13 14:26:00 -0500

    Convert the Blog Post Page

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -8,12 +8,7 @@
                <div class="mt-8 flex flex-col justify-between">
                   <header>
                      <div class="space-x-2">
-                        <a href="#"
-                           class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                           style="font-size: 10px">{{$post->category->slug}}</a>
-                        <a href="#"
-                           class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                           style="font-size: 10px">{{$post->category->name}}</a>
+                        <x-cat-button :category="$post->category" />
                      </div>
                      <div class="mt-4">
                         <h1 class="text-3xl">
