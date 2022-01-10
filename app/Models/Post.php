b757a660a1a9e703dcffa825df4454d6c1fc2efd
Jeff Tocco <jnt369@gmail.com>
2022-01-10 11:56:14 -0500

    Make the Comments Section Dynamic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,6 +25,12 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+
     public function scopeFilter($query, array $filters) //Post::newQuery()->filter()
     {
         $query->when($filters['search'] ?? false, fn($query, $search) =>
